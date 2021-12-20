fdd7fcef0823acc603054f0af28991e9428a1470
Saharat wongjan <644230056@webmail.npru.ac.th.com>
2021-12-20 15:50:41 +0700

    second commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,7 @@
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Helloworld");
+        System.out.println("Hello Saharat");
+        System.out.println("Hello wongjan");
     }
 }
